9373f1c346dc90dc0719783a351c578fc9b890ae
Kai Londenberg <klondenberg@meta.com>
2023-12-14 19:07:26 +0100

    [inductor max autotune] Improvements to EVT codegen, broadcasting of aux loads and activations
    Simple change adding support for Sigmoid and Tanh activations. Several improvements to
    EVT codegen, specifically to make broadcasting of aux loads possible.
    
    Test Plan:
    
     * CI
     * Additional unit test
    
    ghstack-source-id: 84192a220b722127eca5fec152218d70985d52e2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/114606

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -1218,10 +1218,6 @@ class WrapperCodeGen(CodeGen):
         )
 
     def codegen_allocation(self, buffer):
-        assert (
-            buffer.get_workspace_size() == 0
-        ), "Only support zero workspace size for now!"
-
         name = buffer.get_name()
 
         if name in V.graph.removed_buffers or name in self.allocated:
@@ -1247,10 +1243,6 @@ class WrapperCodeGen(CodeGen):
         self.writeline(AllocateLine(self, buffer))
 
     def codegen_free(self, buffer):
-        assert (
-            buffer.get_workspace_size() == 0
-        ), "Only support zero workspace size for now!"
-
         name = buffer.get_name()
 
         # can be freed but not reused
